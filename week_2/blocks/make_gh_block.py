68def76efb88e45908dc10cd57bdb6e7bcfdabf9
Master-Leo <emillio3198@gmail.com>
2023-04-01 13:19:08 -0700

    uploaded data

---
+++ week_2/blocks/make_gh_block.py
@@ -0,0 +1,8 @@
+# from prefect.filesystems import GitHub
+
+# block = GitHub(
+#     nmame = 'de-zoom',
+#     repository="https://github.com/Master-Leo/data-engineering-course", 
+# )
+
+# block.save("de-zoom", overwrite=True)
